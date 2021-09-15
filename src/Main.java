634acba61edabca2fa1a96fa0ea68f590bc2c924
Hamedxoxo <31958217+Hamedxoxo@users.noreply.github.com>
2021-09-15 11:56:57 +0200

    Greeting + question + userInput

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	String greeting = "Hello World!";
+    Scanner userinput = new Scanner(System.in);
 
     System.out.println(greeting);
     System.out.println("Anything you want me to know human?");
+    String needToKnow = userinput.nextLine();
+    System.out.println("Okay so, " + needToKnow);
+
+
     }
 }
