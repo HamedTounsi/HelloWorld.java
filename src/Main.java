74f64c641bf1338c9ff26b78e44876ac14b90d92
Hamedxoxo <31958217+Hamedxoxo@users.noreply.github.com>
2021-09-15 11:49:01 +0200

    Greeting

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	String greeting = "Hello World!";
 
-        System.out.println(greeting);
+    System.out.println(greeting);
+    System.out.println("Anything you want me to know?");
     }
 }
